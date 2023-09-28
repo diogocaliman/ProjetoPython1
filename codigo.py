4dde0fbd92c3f831d1fe1abf7f8b263c9f15eb60
Diogo Caliman <diogocaliman@outlook.com>
2023-09-28 15:05:24 -0300

    Primeiro Commit

---
+++ codigo.py
@@ -0,0 +1,31 @@
+# Passo a passo do projeto 
+
+# 1 : Entrar no Sistema
+
+import pyautogui
+
+# pyautogui.click -> clicar com o mouse
+# pyautogui.write -> escrever um texto
+# pyautogui.press -> apertar 1 tecla
+# pyautogui.hotkey -> atalho
+
+pyautogui.PAUSE = 0.5
+
+# abrir o chrome
+
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+
+# entrar no link
+# esperar o site carregar
+
+
+# 2 : Fazer login
+# 3 : Importar os dados do produto
+# 4 : Cadastro
+# 5 : Repetir o cadastro
+
+
+
